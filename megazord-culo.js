6143b70c89eba08ed5701aec54dfce502ea71501
LuCulo <LuCosa@Lucias-MacBook-Pro.local>
2016-12-14 20:54:44 -0300

    One js at init, other for the culo

---
+++ megazord-culo.js
@@ -0,0 +1,4 @@
+// No scalability, just get the job done and create functions as needed
+
+
+document.getElementById('Image-culo').src='http://www.cdc.gov/std/spanish/gonorrea/images/gonomain.jpg'
--- megazord.js
+++ megazord.js
@@ -1,1 +1,4 @@
-document.getElementById('Image-culo').src='http://www.cdc.gov/std/spanish/gonorrea/images/gonomain.jpg'
+// No scalability, just get the job done and create functions as needed
+
+
+console.log("VIVA PERON")
