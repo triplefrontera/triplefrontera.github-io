30a624397b98ab521edc577c1aa608d7106929f5
LuCulo <LuCosa@Lucias-MacBook-Pro.local>
2016-12-14 20:57:57 -0300

    a ver si anda sin guion medio...

--- megazordculo.js
+++ megazordculo.js
@@ -1,4 +1,4 @@
 // No scalability, just get the job done and create functions as needed
 
-
+console.log("VIVAN LOS CULOS")
 document.getElementById('Image-culo').src='http://www.cdc.gov/std/spanish/gonorrea/images/gonomain.jpg'
