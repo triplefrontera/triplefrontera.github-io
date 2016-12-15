74a087637da3fe890479b5bceeca5b63dba8dd33
LuCulo <LuCosa@Lucias-MacBook-Pro.local>
2016-12-14 21:20:22 -0300

    ya casi

--- megazord.js
+++ megazord.js
@@ -11,10 +11,10 @@ function f1() {
 
 
 function culo() {
-    document.getElementById('Image-culo').src='http://www.cdc.gov/std/spanish/gonorrea/images/gonomain.jpg'
+    document.getElementById('Image-culo').src='http://www.cdc.gov/std/spanish/gonorrea/images/gonomain.jpg';
     console.log("VIVAN LOS CULOS");
 }
 
 window.onload = function() {
-    document.getElementById("btn-culo").onclick = culo();
+    document.getElementById("btn-culo").onclick = culo()
 }
