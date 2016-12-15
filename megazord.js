164cf2e54fabd6563fe6d0310f65c718022f5ef1
LuCulo <LuCosa@Lucias-MacBook-Pro.local>
2016-12-14 22:04:02 -0300

    last acceptable state. TODO: take text from acceptable format and generate content

--- megazord.js
+++ megazord.js
@@ -4,25 +4,11 @@
 console.log("VIVA PERON")
 var culol = 0;
 
-/*'<p>But... should toasts follow the same rule?\n\nI would say it\'s 
-				pretty reasonable to have smoke coming out of a damn toaster, dude. \n\n
-				What do product people have to say about this?</p>
-				<br>
-				<p>Wikipedia, however, is not so romantic: The term "smoke test" refers to 
-				powering on a device simply to make sure it does not start smoking (indicating 
-					a major problem). It originated in the testing of electronic hardware</p>',*/
+
 
 //var templateChapter = {"imageURL":"link","text":"<p> Text </p> <br><p> Text </p>","footer":"text"};
 var chapter = {"imageURL":"http://www.cdc.gov/std/spanish/gonorrea/images/gonomain.jpg",
-				"textHTML":
-				'<p>But... should toasts follow the same rule?\n\nI would say it\'s 
-				pretty reasonable to have smoke coming out of a damn toaster, dude. \n\n
-				What do product people have to say about this?</p>
-				<br>
-				<p>Wikipedia, however, is not so romantic: The term "smoke test" refers to 
-				powering on a device simply to make sure it does not start smoking (indicating 
-					a major problem). It originated in the testing of electronic hardware</p>'
-				,
+				"textHTML": '<p> Text </p> <br><p> Text </p>',
 				"footer":"text"};
 var content = [];
 
