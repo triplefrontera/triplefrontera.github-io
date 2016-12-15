3d7defb7bc24eb40005ee721fffb0a8360ef45f6
LuCulo <LuCosa@Lucias-MacBook-Pro.local>
2016-12-14 21:08:29 -0300

    aslkj

--- megazord.js
+++ megazord.js
@@ -2,3 +2,16 @@
 
 
 console.log("VIVA PERON")
+
+
+function f1() {
+    alert("f1 called");
+    //form validation that recalls the page showing with supplied inputs.    
+}
+window.onload = function() {
+    document.getElementById("btn-culo").onclick = function fun() {
+        alert("hello");
+        f1();
+        //validation code to see State field is mandatory.  
+    }
+}
