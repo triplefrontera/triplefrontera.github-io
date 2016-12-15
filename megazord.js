2c972963471b48db0fcf72c0964303a70d57345f
LuCulo <LuCosa@Lucias-MacBook-Pro.local>
2016-12-14 21:40:32 -0300

    eeee

--- megazord.js
+++ megazord.js
@@ -9,7 +9,7 @@ var imagenes = [];
 
 function culo() {
     document.getElementById('Image-culo').src='http://www.cdc.gov/std/spanish/gonorrea/images/gonomain.jpg';
-    ducoment.getElementById('Text-culo').innerHTML="<p> test </p><br><p> test </p>";
+    document.getElementById('Text-culo').innerHTML="<p> test </p><br><p> test </p>";
     console.log("VIVAN LOS CULOS number "+culol);
 }
 
